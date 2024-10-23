8be7f75ebb24046bba790557046e41e53ce72912
Blixt <me@blixt.nyc>
2024-10-23 20:00:51 +0200

    Make sure writes and close of exposed client channel happen on single goroutine

--- hotel/client.go
+++ hotel/client.go
@@ -8,6 +8,7 @@ import (
 
 type Client[ClientMetadata any, MessageType any] struct {
 	metadata  *ClientMetadata
+	bufferCh  chan MessageType
 	sendCh    chan MessageType
 	ctx       context.Context
 	cancel    context.CancelFunc
@@ -16,12 +17,37 @@ type Client[ClientMetadata any, MessageType any] struct {
 
 func newClient[ClientMetadata any, MessageType any](metadata *ClientMetadata) *Client[ClientMetadata, MessageType] {
 	ctx, cancel := context.WithCancel(context.Background())
-	return &Client[ClientMetadata, MessageType]{
+	c := &Client[ClientMetadata, MessageType]{
 		metadata: metadata,
-		sendCh:   make(chan MessageType, 256),
+		bufferCh: make(chan MessageType, 256),
+		sendCh:   make(chan MessageType),
 		ctx:      ctx,
 		cancel:   cancel,
 	}
+	// Forward messages sent to sendCh (from any goroutine) to a channel that is
+	// synchronized to a single goroutine.
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				close(c.sendCh)
+				return
+			case msg := <-c.bufferCh:
+				// Forwarding to sendCh will always block until the user code
+				// has read from the Receive() channel. If the buffer channel
+				// fills up, then the send method will close the client, which
+				// is why we also check the context here.
+				select {
+				case <-ctx.Done():
+					close(c.sendCh)
+					return
+				case c.sendCh <- msg:
+					// All good, keep going.
+				}
+			}
+		}
+	}()
+	return c
 }
 
 func (c *Client[ClientMetadata, MessageType]) Context() context.Context {
@@ -36,7 +62,7 @@ func (c *Client[ClientMetadata, MessageType]) send(message MessageType) error {
 	select {
 	case <-c.ctx.Done():
 		return errors.New("client disconnected")
-	case c.sendCh <- message:
+	case c.bufferCh <- message:
 		return nil
 	default:
 		// Channel is full, disconnect the client
@@ -46,13 +72,12 @@ func (c *Client[ClientMetadata, MessageType]) send(message MessageType) error {
 }
 
 func (c *Client[ClientMetadata, MessageType]) Receive() <-chan MessageType {
+	// Return the channel that only the internal client goroutine writes to.
 	return c.sendCh
 }
 
 func (c *Client[ClientMetadata, MessageType]) Close() {
 	c.closeOnce.Do(func() {
 		c.cancel()
-		// FIXME: Writes and closes of sendCh have to happen on same goroutine/under lock.
-		close(c.sendCh)
 	})
 }
