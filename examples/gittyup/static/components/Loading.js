a0558c5193f7caf0c5c6f07f281a984241effbbe
Blixt <me@blixt.nyc>
2024-11-02 17:11:25 +0100

    Remove examples from repo (moved to other repo)

--- examples/gittyup/static/components/Loading.js
+++
@@ -1,8 +0,0 @@
-import htm from "htm";
-import React from "react";
-
-const html = htm.bind(React.createElement);
-
-export function Loading() {
-    return html`<div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>`;
-}
--- go.mod
+++ go.mod
@@ -4,7 +4,4 @@ go 1.23.2
 
 require github.com/gorilla/websocket v1.5.3
 
-require (
-	github.com/joho/godotenv v1.5.1
-	golang.org/x/sync v0.8.0
-)
+require golang.org/x/sync v0.8.0
